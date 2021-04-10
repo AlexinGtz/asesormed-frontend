aee25f9244f997d39267ef16257c6e7a6dc8100c
Alejandro Gutierrez <66271973+AlexinGtz@users.noreply.github.com>
2021-04-10 17:03:43 -0500

    Update messageTypes.js

--- src/messageTypes.js
+++ src/messageTypes.js
@@ -1,4 +1,4 @@
 export const MESSAGE_TYPE_SUCCESS = 1;
 export const MESSAGE_TYPE_ERROR = 0;
 //export const CURRENT_ROUTE = "localhost:3001";
-export const CURRENT_ROUTE = "45.90.109.60:3001";
+export const CURRENT_ROUTE = "a0brvip3n7.execute-api.us-east-2.amazonaws.com/dev";
